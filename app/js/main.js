d10f7081502493609e0f888809d65c684fb1584e
heindesign <hein@yandex.com>
2018-12-01 20:51:51 +0500

    portfolio done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+/* owl carousel */
+
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 		items: 1,
@@ -6,3 +8,30 @@ $(document).ready(function(){
 		autoplayTimeout: 5000
 	});
 });
+
+/**/
+
+/* isotope */
+
+$(window).on("load", function() {
+	$(".sort-by__item").on("click", function() {
+		$(".sort-by__item").removeClass("active");
+		$(this).addClass("active");
+
+		var data = $(this).attr("data-filter");
+		$workGrid.isotope({
+			filter: data
+		});
+	});
+	if (document.getElementById("work")) {
+		var $workGrid = $(".projects").isotope({
+			itemSelector: ".grid-item",
+			percentPosition: true,
+			masonry: {
+				columnWidth: ".grid-sizer"
+			}
+		});
+	}
+});
+
+/**/
